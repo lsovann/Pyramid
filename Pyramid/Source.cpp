10aa51a8691ba43e0014a48857efc34a341cd29b
Sovann Lyhour <sovannlyhour8@gmail.com>
2017-06-17 15:49:13 +0700

    Add getArea(), digonal_distance(), isSamePoint(),and isRealistic()

--- Pyramid/Source.cpp
+++ Pyramid/Source.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdool.h>
+#include <stdbool.h>
+#include <cmath>
 
 struct point
 {
@@ -14,11 +15,30 @@ struct pyramid
 	double height;
 };
 
+double digonal_distance(struct point p1, struct point p2);
+bool isSamePoint(struct point p1, struct point p2);
+bool isRealistic(struct pyramid p);
+double getArea(struct pyramid p);
+double getVolume(struct pyramid p);
 int main(void)
 {
-
+	struct pyramid p1 = {
+		{2, 0, 0},
+		{2, 2, 0},
+		2
+	};
+	if (isRealistic(p1)) {
+		printf("The Area of p1 is: %f", getArea(p1));
+	}
+	getchar();
 	return 0;
 }//end of main
+
+double digonal_distance(struct point p1, struct point p2)
+{
+	double distance = pow(pow(p2.x - p1.x,2) + pow(p2.y - p1.y,2) + pow(p2.z - p1.z, 2),0.5);
+	return distance;
+}//end of find digonal
 
 bool isSamePoint(struct point p1, struct point p2)
 {
@@ -28,28 +48,36 @@ bool isSamePoint(struct point p1, struct point p2)
 		samePoint = true;
 	}
 	return samePoint;
-}
+}//end of check samePoint
 
 //check if points can form pyramid
-bool isRealistic(struct pyramid p, double h)
+bool isRealistic(struct pyramid p)
 {
 	bool realistic = false;
-	if (!isSamePoint(p.base_point1, p.base_point2) && h < 0)
+	if (!isSamePoint(p.base_point1, p.base_point2) && p.height > 0)
 	{
 		realistic = true;
 	}
 	return realistic;
-
 }//end of isRealistic
 
  //calculate area from every surface
-double getArea(struct pyramid p, double height)
+double getArea(struct pyramid p)
 {
+	double base_area;
+	double surface_area;
+	double total_area;
+	double base_vertex = digonal_distance(p.base_point1, p.base_point2) / pow(2, 0.5);
 
+	base_area = pow(base_vertex, 2);
+	surface_area = (pow(pow(p.height, 2) + pow(base_vertex/2, 2), 0.5)*base_vertex) / 2;
+
+	total_area = 4 * surface_area + base_area;
+	return total_area;
 }//end of getArea
 
  //calculate volume of pyramid
 double getVolume(struct pyramid p)
 {
-
+	return 0.4;
 }//end of getVolume
