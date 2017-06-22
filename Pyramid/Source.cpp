15665da9236643dd978f7f03aa9a934ab53037e8
Sovann Lyhour <sovannlyhour8@gmail.com>
2017-06-22 18:14:38 +0700

    Add getPeritmeter, add normal vector, optimise codes

--- Pyramid/Source.cpp
+++ Pyramid/Source.cpp
@@ -8,14 +8,21 @@ struct point
 	double y;
 	double z;
 };
+
+struct vector
+{
+	double i;
+	double j;
+	double k;
+};
 struct pyramid
 {
 	struct point base_point1;
 	struct point base_point2;
 	double height;
+	struct vector normal;
 };
 
-double digonal_distance(struct point p1, struct point p2);
 bool isSamePoint(struct point p1, struct point p2);
 bool isRealistic(struct pyramid p);
 double getArea(struct pyramid p);
@@ -28,18 +35,12 @@ int main(void)
 		2
 	};
 	if (isRealistic(p1)) {
-		printf("The Area of p1 is: %f\n", getArea(p1));
-		printf("The Volume of p1 is: %f", getVolume(p1));
+		printf("The Area of p1 is: %.2f\n", getArea(p1));
+		printf("The Volume of p1 is: %.2f", getVolume(p1));
 	}
 	getchar();
 	return 0;
 }//end of main
-
-double digonal_distance(struct point p1, struct point p2)
-{
-	double distance = pow(pow(p2.x - p1.x,2) + pow(p2.y - p1.y,2) + pow(p2.z - p1.z, 2),0.5);
-	return distance;
-}//end of find digonal
 
 bool isSamePoint(struct point p1, struct point p2)
 {
@@ -51,30 +52,43 @@ bool isSamePoint(struct point p1, struct point p2)
 	return samePoint;
 }//end of check samePoint
 
+//check if normal vector perpendicular with base surface, return true if it perpendicular
+bool isPerpendicular(struct vector n, struct point baseP1, struct point baseP2)
+{
+
+}// end of isPerpendicular
+
 //check if points can form pyramid
 bool isRealistic(struct pyramid p)
 {
 	bool realistic = false;
-	if (!isSamePoint(p.base_point1, p.base_point2) && p.height > 0)
+	if (!isSamePoint(p.base_point1, p.base_point2) && p.height > 0 && isPerpendicular(p.normal, p.base_point1, p.base_point2))
 	{
 		realistic = true;
 	}
 	return realistic;
 }//end of isRealistic
 
+double getBaseLength(struct point p1, struct point p2)
+{
+	double digonal = pow(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2) + pow(p2.z - p1.z, 2), 0.5);
+	return digonal/pow(2, 0.5);
+}//end of getBaseLength
+
+double pythagor_equation(double a, double b)
+{
+	return pow(pow(a, 2) + pow(b, 2), 0.5);
+}
+
  //calculate area from every surface
 double getArea(struct pyramid p)
 {
 	double base_area;
 	double surface_area;
-	double total_area;
-	double base_vertex = digonal_distance(p.base_point1, p.base_point2) / pow(2, 0.5);
 
-	base_area = pow(base_vertex, 2);
-	surface_area = (pow(pow(p.height, 2) + pow(base_vertex/2, 2), 0.5)*base_vertex) / 2;
-
-	total_area = 4 * surface_area + base_area;
-	return total_area;
+	base_area = pow(getBaseLength(p.base_point1, p.base_point2), 2);
+	surface_area = pythagor_equation(p.height, getBaseLength(p.base_point1, p.base_point2)/2)*getBaseLength(p.base_point1, p.base_point2) / 2;
+	return 4 * surface_area + base_area;
 }//end of getArea
 
  //calculate volume of pyramid
@@ -82,9 +96,33 @@ double getVolume(struct pyramid p)
 {
 	double base_area;
 	double volume;
-	double base_vertex = digonal_distance(p.base_point1, p.base_point2) / pow(2, 0.5);
 
-	base_area = pow(base_vertex, 2);
-	volume = (base_vertex * p.height) / 3;
+	base_area = pow(getBaseLength(p.base_point1, p.base_point2), 2);
+	volume = (base_area * p.height) / 3;
 	return volume;
 }//end of getVolume
+
+double getPerimeter(struct pyramid p)
+{
+	double triangle_height = pythagor_equation(p.height, getBaseLength(p.base_point1, p.base_point2) / 2);
+	double base_length = getBaseLength(p.base_point1, p.base_point2);
+	double triangle_side = pythagor_equation(triangle_height, base_length/2);
+	return 4 * base_length + 4 * triangle_side;
+}//end of getPerimeter
+
+struct point getBasePoint3(struct pyramid p)
+{
+	//calculate base point3
+}
+
+struct point getBasePoint4(struct pyramid p)
+{
+	//calculate base point4
+}
+
+/*
+Sokhavirith's tasks:
+- complete fn isPerpendicular()
+- complete fn getBasePoint3()
+- complete fn getBasePoint4()
+*/
